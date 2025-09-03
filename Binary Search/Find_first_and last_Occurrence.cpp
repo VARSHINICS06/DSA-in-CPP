2e23d56c42678d27f35d9d1f96071bc649b0384f
Varshini C S <varshinics123@gmail.com>
2025-09-03 14:04:17 +0530

    Add files via upload

---
+++ Binary Search/Find_first_and last_Occurrence.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+public:
+    int firstOcc(vector<int> &arr, int n, int k)
+    {
+        int low = 0, high = n - 1;
+        int first = -1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (arr[mid] == k)
+            {
+                first = mid;
+                high = mid - 1;
+            }
+            else if (arr[mid] < k)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return first;
+    }
+    int lastOcc(vector<int> &arr, int n, int k)
+    {
+        int low = 0, high = n - 1;
+        int last = -1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (arr[mid] == k)
+            {
+                last = mid;
+                low = mid + 1;
+            }
+            else if (arr[mid] < k)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return last;
+    }
+    pair<int, int> searchRange(vector<int> &arr, int n, int k)
+    {
+        int first = firstOcc(arr, n, k);
+        if (first == -1)
+            return {-1, -1};
+        int last = lastOcc(arr, n, k);
+        return {first, last};
+    }
+};
